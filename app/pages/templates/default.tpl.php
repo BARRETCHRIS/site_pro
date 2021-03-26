d76e390f11119d6ac9b3c9c5247684e3d0948d58
BARRETCHRIS <christelle.barret.poirot@gmail.com>
2021-03-26 10:01:48 +0100

    première version

---
+++ app/pages/templates/default.tpl.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Site personnel</title>
+
+<!-- meta staf -->
+    <meta name="author" content="Christelle BARRET (Crypto-Beasty)">
+    <meta name="designer" content="Christelle BARRET (Crypto-Beasty)">
+    <meta name="graphiste" content="Christelle BARRET (Crypto-Beasty)">
+    <meta name="description" value="
+     e88~-_                             d8                  888~~\                               d8           
+     d888   \ 888-~\ Y88b  / 888-~88e  _d88__  e88~-_        888   |  e88~~8e    /~~~8e   d88~\ _d88__ Y88b  / 
+     8888     888     Y888/  888  888b  888   d888   i       888 _/  d888  88b       88b C888    888    Y888/  
+     8888     888      Y8/   888  8888  888   8888   |       888  \  8888__888  e88~-888  Y88b   888     Y8/   
+     Y888   / 888       Y    888  888P  888   Y888   '       888   | Y888    , C888  888   888D  888      Y    
+     "88_-~  888       /     888-_88"   "88_/  "88_-~        888__/   "88___/   "88_-888 \_88P   "88_/   /     
+                     _/      888                                                                       _/      
+    ">
+     
+<!-- Bootstrap4.6.0 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+<!-- CSS -->
+<link rel="stylesheet" href="styles/default.css">
+<!--<link rel="stylesheet" href="styles/stylesheet.css">-->
+
+    
+</head>
+<body>
+<!-- ***** HEADER ***** -->     
+     <header>
+     <?php include("_default_header.php"); ?>
+
+     </header>
+<!-- ***** CONTENT ***** -->
+     <div class="wrapper"> 
+          <main>
+               <?php echo $content; ?>     
+          </main>
+          
+     </div>
+<!-- ***** FIN FOOTER ***** -->
+<footer class="txt-gold">
+          <?php include("_default_footer.php"); ?>        
+</footer>
+<!-- ********** JS ********** -->
+<!-- jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    
+</body>
+</html>
